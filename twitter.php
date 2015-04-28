efcc621a710bc6bd31b8c74ef2751a3d620eb6c9
markwalet <mark.walet@gmail.com>
2015-04-28 13:56:36 +0200

    created twitter handler

--- twitter.php
+++ twitter.php
@@ -8,24 +8,17 @@ $connection = new tmhOAuth(array(
 ));
 $parameters = array();
 
-if ($_GET['count']) {
-    $parameters['count'] = strip_tags($_GET['count']);
-}
-
-if ($_GET['screen_name']) {
-    $parameters['screen_name'] = strip_tags($_GET['screen_name']);
-}
-
-if ($_GET['twitter_path']) { $twitter_path = $_GET['twitter_path']; }  else {
-    $twitter_path = '1.1/statuses/user_timeline.json';
-}
+$parameters['geocode'] = (isset($_GET['geocode'])) ? $_GET['geocode'] : "";
+$parameters['q'] = (isset($_GET['q'])) ? $_GET['q'] : "";
+$parameters['count'] = (isset($_GET['count'])) ? $_GET['count'] : 10;
+$twitter_path = '1.1/search/tweets.json';
 
 $http_code = $connection->request('GET', $connection->url($twitter_path), $parameters );
 
 if ($http_code === 200) { // if everything's good
     $response = strip_tags($connection->response['response']);
 
-    if ($_GET['callback']) { // if we ask for a jsonp callback function
+    if (isset($_GET['callback'])) { // if we ask for a jsonp callback function
         echo $_GET['callback'],'(', $response,');';
     } else {
         echo $response;
