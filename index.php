79af1b63a023b14ce14102d420663b978e351c19
MarkKremer <github@markkremer.nl>
2015-04-28 15:18:03 +0200

    CIRCLES!

--- index.php
+++ index.php
@@ -80,6 +80,19 @@
                                 icon: 'images/' + type + '.png'
                             });
 
+                            // Create a radius on location
+                            var populationOptions = {
+                              strokeColor: '#FF0000',
+                              strokeOpacity: 0.8,
+                              strokeWeight: 2,
+                              fillColor: '#FF0000',
+                              fillOpacity: 0.35,
+                              map: map,
+                              center: cords,
+                              radius: 5000
+                            };
+                            cityCircle = new google.maps.Circle(populationOptions);
+
                             google.maps.event.addListener(delictMarker, 'click', function() {
                                 
                                 var contentString = 
